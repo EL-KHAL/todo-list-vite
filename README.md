1a91b5305b6a820617590221629a153c4db80f6d
EL-KHAL <majiddevweb@gmail.com>
2022-07-01 00:17:04 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+![plot](./src/img/todo_khalo.png)
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone 
+```
+
+Go to the project directory
+
+```bash
+  cd TODO-KHALO
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run dev ya khali
+```
---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="content">
+    <TodoList />
+  </div>
+</template>
+
+<script setup>
+import TodoList from "./components/TodoList.vue";
+</script>
+
+<style lang="scss">
+// global ya khali
+:root {
+  --background-color: #070707;
+  --text-primary: #050504;
+  --text-secondary: #030303;
+  --todo-bg-color: #fc95d9;
+  
+}
+@import url('https://fonts.googleapis.com/css2?family=Kanit:ital,wght@1,500&display=swap');
+body {
+  margin: 0px;
+  font-family: 'Kanit', sans-serif;
+}
+.content {
+  background: var(--background-color);
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+}
+</style>
+
